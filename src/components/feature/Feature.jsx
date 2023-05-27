ae969c3adecb56aa12f9252e742419faf6d559ad
IsaacMgr <gauravthapa926@gmail.com>
2023-05-27 23:09:00 +0545

    commit

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './feature.css';
-const Feature = () => {
+//props stands for properties.
+//props are arguments passed into the react components via html attributes
+//react props are like function arguments in js and attributes in html
+
+const Feature = ({title,text}) => {
   return (
-    <div>
-      Feature
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div/>
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
+      </div>
     </div>
   )
 }
--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './features.css';
+//props stands for properties.
+//props are arguments passed into the react components via html attributes
+//react props are like function arguments in js and attributes in html
+
 const Features = () => {
   return (
-    <div>
-      Features
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text"></h1>
+      </div>
+      
     </div>
   )
 }
